f9785ffdaac55a7c678dadc74e8613148d521b75
hy99ee <hy99ee@gmail.com>
2021-12-23 16:54:22 +0300

    Initial Commit

---
+++ ImageViewerStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageViewerStoryboard
+//
+//  Created by Hy99ee on 23.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

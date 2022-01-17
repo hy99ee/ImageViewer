c9f6889578f68c047f46e2e265b646403c8922ea
hy99ee <hy99ee@gmail.com>
2022-01-18 01:23:01 +0300

    Resizing image and scroll view

--- ImageViewerStoryboard/View/Subviews/ImageView.swift
+++ ImageViewerStoryboard/View/Subviews/ImageView.swift
@@ -8,9 +8,12 @@
 import Foundation
 import UIKit
 import SDWebImage
+import RxSwift
+import RxCocoa
 
 final class ImageView: UIImageView {
 
+    var heightImage = BehaviorRelay<CGFloat>(value: 0)
     var unsplashPhoto:UnsplashPhoto?{
         didSet{
             let photoUrl = unsplashPhoto?.urls[UnsplashPhoto.URLSizes.regular.rawValue]
@@ -26,11 +29,26 @@ final class ImageView: UIImageView {
     
     override init(frame: CGRect = UIScreen.main.bounds) {
         super.init(frame: frame)
-        contentMode = .scaleAspectFill
+        contentMode = .scaleAspectFit
         translatesAutoresizingMaskIntoConstraints = false
+        
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var intrinsicContentSize: CGSize
+    {
+
+        let frameSizeWidth = self.frame.size.width
+        guard let image = self.image else
+        {
+            return CGSize(width: frameSizeWidth, height: 1.0)
+        }
+
+        let returnHeight = ceil(image.size.height * (frameSizeWidth / image.size.width))
+        heightImage.accept(returnHeight)
+        return CGSize(width: frameSizeWidth, height: returnHeight)
     }
     
 }
